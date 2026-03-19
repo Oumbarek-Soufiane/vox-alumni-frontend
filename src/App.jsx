d162c53433889fad3697b11da934f1ba7197cfa2
unknown <soufianeoump6@gmail.com>
2026-03-19 03:34:59 +0000

    force clean build pathb

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function Gallery() {
       {/* ── Footer ── */}
       <footer style={s.footer}>
         <img
-          src="/img/Artboard 1@4x.png"
+          src="/img/walid3.jpeg"
           alt="VOX Alumni"
           style={{ height: 140, width: 'auto', marginBottom: 10, objectFit: 'contain', opacity: 0.6 }}
         />
